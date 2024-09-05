b8bcda1c047e04b7be8cd07237ab664b72e05e07
Rahatul99 <rahatul99@gmail.com>
2024-09-05 10:43:24 +0600

    app.mountpath

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require("express");
-const handle = require("./handle");
 
 const app = express();
+const admin = express();
 
-app.locals.title = "My App";
-
-app.get("/", handle);
-
-app.post("/", (req, res) => {
-  res.send("This is home page with post request");
+admin.get("/dashboard", (req, res) => {
+  console.log(admin.mountpath);
+  res.send("Welcome to the admin dashboard");
 });
+
+app.get("/", (req, res) => {
+  res.send("This is home page with get request");
+});
+
+app.use("/admin", admin);
 
 app.listen(5001, () => {
   console.log("listening on port 5000");
