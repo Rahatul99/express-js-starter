59282a96b5385a52c9ac36aadf7a2e5dbe27df5f
Rahatul99 <rahatul99@gmail.com>
2024-09-05 11:04:52 +0600

    app.all method

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
 const express = require("express");
 
 const app = express();
-const admin = express();
 
-admin.on("mount", (parent) => {
-  console.log("Admin Mounted");
-  console.log(parent); // refers to the parent app
-});
-
-admin.get("/dashboard", (req, res) => {
-  console.log(admin.mountpath);
-  res.send("Welcome to the admin dashboard");
-});
-
-app.get("/", (req, res) => {
+app.all("/", (req, res) => {
+  //after set app.all it will be accessible to all the methods
   res.send("This is home page with get request");
 });
-
-app.use("/admin", admin);
 
 app.listen(5001, () => {
   console.log("listening on port 5000");
