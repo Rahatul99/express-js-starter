22a2b19fcaa91a5d02852219271721e712d1c267
Rahatul99 <rahatul99@gmail.com>
2024-09-08 17:12:51 +0600

    res.local

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 const express = require("express");
 const handler = require("./handler");
 
-//cookie parser
-const cookieParser = require("cookie-parser");
-
 const app = express();
 app.use(express.json()); //before use this we got the req.body undefined.but after set this parser i got the real data
-// and use cookieParser
-app.use(cookieParser());
+app.set("view engine", "ejs");
 
-const adminRoute = express.Router();
-
-adminRoute.get("/dashboard", (req, res) => {
-  console.log(req.path);
-  res.send("We are in admin dashboard");
+app.get("/about", (req, res) => {
+  res.render("pages/about", {
+    name: "Bangladesh",
+  });
 });
-
-app.use("/admin", adminRoute);
 
 app.get("/user/:id", handler);
 app.post("/user/", (req, res) => {
