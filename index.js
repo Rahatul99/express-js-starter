8d9bd54f77d2485e7743cc74a43e7f6d2f07e6d8
Rahatul99 <rahatul99@gmail.com>
2024-09-08 12:31:05 +0600

    used pareser

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json()); //before use this we got the req.body undefined.but after set this i got the real data
 
 const adminRoute = express.Router();
 
 adminRoute.get("/dashboard", (req, res) => {
-  console.log(req.originalUrl, "admin"); //result: /admin/dashboard
-  console.log(req.url); //result: /dashboard
+  console.log(req.path);
   res.send("We are in admin dashboard");
 });
 
 app.use("/admin", adminRoute);
 
 app.get("/user/:id", (req, res) => {
-  console.log(req.originalUrl, "app"); //result: /user/1
-  console.log(req.url); //result: /user/1
+  console.log(req.path);
+  console.log(req.body);
   res.send("Hello world");
 });
 
