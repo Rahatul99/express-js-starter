f59c8ad697a5d30f64fc80c11abe469493836e06
Rahatul99 <rahatul99@gmail.com>
2024-09-09 15:48:44 +0600

    res set, res.get and res.location

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const app = express();
 app.use(express.json()); //before use this we got the req.body undefined.but after set this parser i got the real data
 app.set("view engine", "ejs");
 
+app.get("/test", (req, res) => {
+  res.send("Hello");
+});
 app.get("/about", (req, res) => {
-  res.cookie("name", "Hello world", {});
+  // res.location("/test");
+  // res.redirect("/test");
+  res.set("Platform", "Learn with sumit");
+  console.log(res.get("Platform"));
   res.end();
 });
 
