9608dc0ec2bc0adb3183424bcb9bdadc05dd76c5
Rahatul99 <rahatul99@gmail.com>
2024-09-10 17:08:56 +0600

    route.param

--- publicRouter.js
+++ publicRouter.js
@@ -2,21 +2,25 @@ const express = require("express");
 
 const publicRouter = express.Router();
 
-const log = (req, res, next) => {
-  console.log("I am logged in");
-  next();
-};
+// const log = (req, res, next) => {
+//   console.log("I am logged in");
+//   next();
+// };
 
-publicRouter.all("*", log);
+// publicRouter.all("*", log);
 
-publicRouter.param("user", (req, res, next, id) => {
-  req.user = id === "1" ? "Admin" : "Anonymous";
-  console.log("Hello world");
-  next();
+publicRouter.param((param, option) => (req, res, next, val) => {
+  if (val === option) {
+    next();
+  } else {
+    res.sendStatus(403);
+  }
 });
 
+publicRouter.param("user", "12");
+
 publicRouter.get("/:user", (req, res) => {
-  res.send(`Hello ${req.user}`);
+  res.send(`Hello admin`);
 });
 
 publicRouter.get("/login", (req, res) => {
