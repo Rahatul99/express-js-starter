a3d7140456f72eb40d322055d33b88f829f60d52
Rahatul99 <rahatul99@gmail.com>
2024-09-10 10:37:25 +0600

    3rd middleware we use here it is cookie-pareser

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 
+const cookieParser = require("cookie-parser");
 const app = express();
 
+app.use(cookieParser());
 const adminRouter = express.Router();
 
 const myMiddleware = (req, res, next) => {
