46154a78220b0eb3a6efbdb6a694f421fe0d7fd0
Rahatul99 <rahatul99@gmail.com>
2024-09-11 15:26:16 +0600

    multiple file upload

--- index.js
+++ index.js
@@ -10,9 +10,16 @@ const upload = multer({
   dest: UPLOADS_FOLDER,
 });
 
-app.post("/", upload.array("avatar", 3), (req, res) => {
-  res.send("hello");
-});
+app.post(
+  "/",
+  upload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "gallery", maxCount: 2 },
+  ]),
+  (req, res) => {
+    res.send("hello");
+  }
+);
 
 app.listen(5001, () => {
   console.log("listing on port 5001");
